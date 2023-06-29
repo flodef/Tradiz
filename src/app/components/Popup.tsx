3225c153ac0e5e3fa05df34e662dd6f70946435f
Flo <flodef@free.fr>
2023-06-29 19:24:22 +0200

    Fix popup layout problem if too much data

--- src/app/components/Popup.tsx
+++ src/app/components/Popup.tsx
@@ -22,31 +22,37 @@ export const Popup: FC = () => {
         // <div className={removePopupClass('z-20 opacity-50 bg-gray-900 h-screen w-screen grid absolute')}>
         <div
             className={removePopupClass(
-                'z-30 w-5/6 absolute opacity-100 brightness-100 justify-self-center bg-slate-100 h-fit rounded-2xl self-center blur-none border-black'
+                'z-30 w-5/6 max-h-[90%] overflow-y-auto absolute opacity-100 brightness-100 justify-self-center bg-slate-100 h-fit rounded-2xl self-center blur-none border-black'
             )}
             style={{ borderWidth: 'medium' }}
         >
-            <div className="flex justify-between">
-                <div className="text-2xl truncate font-semibold p-3">{popupTitle}</div>
-                <CloseButton onClose={closePopup} />
+            <div>
+                <div className="flex justify-between">
+                    <div className="text-2xl truncate font-semibold p-3">{popupTitle}</div>
+                    <CloseButton onClose={closePopup} />
+                </div>
+                <Separator />
             </div>
-            <Separator />
-            {popupOptions.map((option, index) =>
-                option ? (
-                    <div
-                        className="active:bg-lime-300 w-full relative flex justify-center py-3 items-center font-semibold text-xl text-center"
-                        key={index}
-                        onClick={() => {
-                            popupAction(option);
-                            closePopup();
-                        }}
-                    >
-                        {option}
-                    </div>
-                ) : (
-                    <Separator key={index} color="border-lime-300" />
-                )
-            )}
+            <div>
+                {popupOptions.map((option, index) =>
+                    option ? (
+                        <div
+                            className="active:bg-lime-300 w-full relative flex justify-center py-3 items-center font-semibold text-xl text-center"
+                            key={index}
+                            onClick={() => {
+                                if (!popupAction) return;
+
+                                popupAction(option, index);
+                                closePopup();
+                            }}
+                        >
+                            {option}
+                        </div>
+                    ) : (
+                        <Separator key={index} color="border-lime-300" />
+                    )
+                )}
+            </div>
         </div>
         // </div>
     );
