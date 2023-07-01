d8520300c898f547a264ba36f19856b6d86f0403
Flo <flodef@free.fr>
2023-07-01 11:11:36 +0200

    Increase wallet icon size

--- src/app/images/WalletIcon.tsx
+++ src/app/images/WalletIcon.tsx
@@ -1,6 +1,6 @@
 import React, { FC, SVGProps } from 'react';
 
-export const WalletIcon: FC<SVGProps<SVGSVGElement>> = ({ width = 36, height = 36 }) => {
+export const WalletIcon: FC<SVGProps<SVGSVGElement>> = ({ width = 56, height = 56 }) => {
     return (
         <svg width={width} height={width} fill="none" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
             <path
