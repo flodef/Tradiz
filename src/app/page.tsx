853462a827ca352f15893ea882614d68f48d1526
Flo <flodef@free.fr>
2023-07-04 03:37:34 +0200

    Put all data as export Add products label with price Remove redundant info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,44 +7,71 @@ import { Total } from './components/Total';
 import { DataProvider } from './contexts/DataProvider';
 import { PopupProvider } from './contexts/PopupProvider';
 
-export default function Home() {
-    const maxDecimals = 2;
-    const maxValue = 999.99;
-    const otherKeyword = 'Autres';
-    const categories = [
-        'Boulange',
-        'Pâtisserie',
-        'Epicerie',
-        'Salon⋅Thé',
-        'Journal',
-        'Alcool',
-        'Cartes Postales',
-        'Tableau',
-        'Couture',
-        'Livres (Patrick)',
-        'Art (Eliot)',
-        'Savons (Morgane)',
-    ];
-    const taxes = [
-        { rate: 5.5, categories: ['Boulange', 'Pâtisserie', 'Epicerie'] },
-        { rate: 10, categories: ['Salon⋅Thé'] },
-        { rate: 20, categories: ['Alcool'] },
-    ];
-    const paymentMethods = ['CB', 'Espèces', 'Chèque', 'Crypto'];
+export const otherKeyword = 'Autres';
 
+export const maxDecimals = 2;
+export const maxValue = 999.99;
+export const inventory = [
+    {
+        category: 'Boulange',
+        rate: 5.5,
+        products: [
+            { label: 'Baguette', price: 1.2 },
+            { label: 'Campagne', price: 3 },
+        ],
+    },
+    {
+        category: 'Pâtisserie',
+        rate: 5.5,
+        products: [
+            { label: 'Croissant', price: 1.3 },
+            { label: 'Chocolatine', price: 1.4 },
+        ],
+    },
+    { category: 'Epicerie', rate: 5.5, products: [{ label: 'Farine', price: 5 }] },
+    {
+        category: 'Salon⋅Thé',
+        rate: 10,
+        products: [
+            { label: 'Café court', price: 1.5 },
+            { label: 'Café allongé', price: 2 },
+            { label: 'Thé', price: 3 },
+        ],
+    },
+    {
+        category: 'Alcool',
+        rate: 20,
+        products: [
+            { label: 'Cidre', price: 5 },
+            { label: 'Bière', price: 2 },
+        ],
+    },
+    {
+        category: otherKeyword,
+        rate: 0,
+        products: [
+            { label: 'Journal', price: 0 },
+            { label: 'Cartes Postales', price: 0 },
+            { label: 'Tableau', price: 0 },
+            { label: 'Couture', price: 0 },
+            { label: 'Livres (Patrick)', price: 0 },
+            { label: 'Art (Eliot)', price: 0 },
+            { label: 'Savons (Morgane)', price: 0 },
+        ],
+    },
+];
+
+export const paymentMethods = ['CB', 'Espèces', 'Chèque', 'Crypto'];
+
+export default function Home() {
     return (
         <main className="absolute inset-0 bg-orange-100 text-amber-600 grid select-none overflow-y-auto">
             <DataProvider>
                 <PopupProvider>
                     <div className="z-10 h-screen flex flex-col justify-between">
-                        <Total maxDecimals={maxDecimals} />
-                        <NumPad
-                            maxDecimals={maxDecimals}
-                            maxValue={maxValue}
-                            paymentMethods={paymentMethods}
-                            taxes={taxes}
-                        />
-                        <Category categories={categories} otherKeyword={otherKeyword} />
+                        <Total />
+                        <NumPad />
+                        <Category />
                     </div>
                     <Popup />
                 </PopupProvider>
