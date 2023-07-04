f4e52346646a9cd4360b076a0ebf615713453616
Flo <flodef@free.fr>
2023-07-04 03:39:19 +0200

    Category offers a list of product with preselected price

--- src/app/components/Category.tsx
+++ src/app/components/Category.tsx
@@ -1,74 +1,107 @@
-import { FC, useCallback } from 'react';
+import { FC, MouseEventHandler, useCallback } from 'react';
 import { useData } from '../hooks/useData';
 import { usePopup } from '../hooks/usePopup';
+import { inventory, otherKeyword } from '../page';
 import { isFullscreen, requestFullscreen } from '../utils/fullscreen';
 import { isMobileDevice } from '../utils/mobile';
 import { useAddPopupClass } from './Popup';
 import { Separator } from './Separator';
 
-interface Categories {
-    categories: string[];
-    otherKeyword: string;
-}
-
 interface CategoryInputButton {
     input: string;
-    onInput: (input: string) => void;
+    onInput: (input: string, eventType: string) => void;
 }
 
 const CategoryButton: FC<CategoryInputButton> = ({ input, onInput }) => {
     const { amount } = useData();
 
-    const onClick = useCallback(() => {
-        if (!isFullscreen() && isMobileDevice()) {
-            requestFullscreen();
-        }
-        if (amount) {
-            onInput(input);
-        }
-    }, [input, onInput, amount]);
+    const onClick = useCallback<MouseEventHandler>(
+        (e) => {
+            e.preventDefault();
+            if (!isFullscreen() && isMobileDevice()) {
+                requestFullscreen();
+            }
 
-    let s = 'w-1/3 relative flex justify-center py-3 items-center font-semibold text-2xl ';
-    s += amount ? 'active:bg-orange-300' : 'text-gray-300';
+            onInput(input, e.type);
+        },
+        [input, onInput]
+    );
 
     return (
-        <div className={s} onClick={onClick}>
+        <div
+            className="w-1/3 relative flex justify-center py-3 items-center font-semibold text-2xl active:bg-orange-300"
+            onClick={onClick}
+            onContextMenu={onClick}
+        >
             {input}
         </div>
     );
 };
 
-export const Category: FC<Categories> = ({ categories, otherKeyword }) => {
-    const { addProduct, amount } = useData();
+export const Category: FC = () => {
+    const { addProduct, amount, setAmount, category, setCategory } = useData();
     const { openPopup } = usePopup();
 
-    const onInput = useCallback(
-        (input: string) => {
-            if (input !== otherKeyword) {
-                addProduct(input);
+    const addCategory = useCallback(
+        (newCategory: string) => {
+            if (!category) {
+                addProduct(newCategory);
             } else {
-                openPopup('Catégorie', categories.slice(5), addProduct);
+                addProduct(category);
+                setCategory(newCategory);
             }
         },
-        [categories, otherKeyword, addProduct, openPopup]
+        [addProduct, category, setCategory]
+    );
+
+    const onInput = useCallback(
+        (input: string, eventType: string) => {
+            const item = inventory.find((item) => item.category === input);
+            if (!item) return;
+
+            if (eventType === 'contextmenu' && amount) {
+                addCategory(item.category);
+            } else {
+                openPopup(
+                    item.category,
+                    item.products.map((product) => product.label).concat(otherKeyword),
+                    (option, index) => {
+                        const newCategory = item.category.concat('>', option);
+                        if (amount) {
+                            addCategory(newCategory);
+                        }
+                        if (!amount || category) {
+                            const price = item.products.at(index)?.price;
+                            if (option !== otherKeyword && price) setAmount(price);
+                            setCategory(newCategory);
+                        }
+                    }
+                );
+            }
+        },
+        [openPopup, amount, setAmount, addCategory, setCategory, category]
     );
 
     return (
-        <div className={useAddPopupClass('inset-x-0 divide-y divide-orange-300' + (amount ? '' : ' invisible'))}>
+        <div className={useAddPopupClass('inset-x-0 divide-y divide-orange-300')}>
             <Separator />
-            {categories.length > 0 && (
+            {inventory.length > 0 && (
                 <div className="flex justify-evenly divide-x divide-orange-300">
-                    {categories.slice(0, 3).map((category) => (
-                        <CategoryButton key={category} input={category} onInput={onInput} />
+                    {inventory.slice(0, 3).map((item, index) => (
+                        <CategoryButton key={index} input={item.category} onInput={onInput} />
                     ))}
                 </div>
             )}
-            {categories.length > 3 && (
+            {inventory.length > 3 && (
                 <div className="flex justify-evenly divide-x divide-orange-300">
-                    {categories.slice(3, 6).map((category) => (
+                    {inventory.slice(3, 6).map((item, index) => (
                         <CategoryButton
-                            key={category}
-                            input={category === categories[5] && categories.length > 6 ? otherKeyword : category}
+                            key={index}
+                            input={
+                                item.category === inventory[5].category && inventory.length > 6
+                                    ? otherKeyword
+                                    : item.category
+                            }
                             onInput={onInput}
                         />
                     ))}
