fc5f41ecebf3819f55ddaeefd31cc0e03b0d90b1
Flo <flodef@free.fr>
2023-07-06 02:51:09 +0200

    Handle screenshot as async to add callback when finished

--- src/app/utils/screenshot.ts
+++ src/app/utils/screenshot.ts
@@ -1,20 +1,19 @@
 import html2canvas from 'html2canvas';
 
-export function takeScreenshot(elementId: string, fileName: string) {
+export async function takeScreenshot(elementId: string, fileName: string) {
     const element = document.getElementById(elementId);
     if (element) {
-        html2canvas(element).then((canvas) => {
-            console.log(canvas);
-            canvas.toBlob((blob) => {
-                if (blob) {
-                    const a = document.createElement('a');
-                    a.href = URL.createObjectURL(blob);
-                    a.download = fileName;
-                    a.click();
-                }
-            });
-            canvas.remove();
+        const canvas = await html2canvas(element);
+
+        canvas.toBlob((blob) => {
+            if (blob) {
+                const a = document.createElement('a');
+                a.href = URL.createObjectURL(blob);
+                a.download = fileName;
+                a.click();
+            }
         });
+        canvas.remove();
     } else {
         console.error('Element not found: ' + elementId);
     }
