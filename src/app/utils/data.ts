e01230522a39e6767abc90058bdfcbf141082f75
Flo <flodef@free.fr>
2023-07-06 20:34:39 +0200

    Fix name bug with incorrect month number

--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -3,7 +3,7 @@ export const categorySeparator = '>';
 export const maxDecimals = 2;
 export const maxValue = 999.99;
 export const currency = '€';
-export const defaultDate = [new Date().getFullYear(), new Date().getMonth(), new Date().getDate()].join('-');
+export const defaultDate = [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()].join('-');
 
 export const inventory = [
     {
