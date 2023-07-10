58dc2b7922a8b1bfbf388dbc3a533005916c0943
Flo <flodef@free.fr>
2023-07-10 23:57:15 +0200

    Move data to the api folder

--- src/app/api/json/route.ts
+++ src/app/api/json/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import parameters from '../../data/parameters.json';
-import products from '../../data/products.json';
+import parameters from '../data/parameters.json';
+import products from '../data/products.json';
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
