12e0af7a1cc47b1edc61f8f7a60be0aa05c6aa94
Flo <flodef@free.fr>
2023-07-10 20:04:41 +0200

    Data are loaded from server Check online status and if offline, load from local

--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -1,21 +1,24 @@
 import { InventoryItem } from '../hooks/useConfig';
-import { GOOGLE_API_KEY, GOOGLE_SPREADSHEET_ID } from './env';
 
-export async function LoadData() {
-    const parameters = await fetch(getData('Paramètres', 'fetchParameters', false))
-        // TODO use next/server route
-        //const parameters = await fetch('./src/app/api/route.ts')
+export async function LoadData(isOnline = true) {
+    if (isOnline && !navigator?.onLine) throw new Error('The web app is offline');
+
+    const parameters = await fetch(
+        isOnline ? './api/spreadsheet?sheetName=Paramètres&isRaw=false' : './api/json?fileName=parameters'
+    )
+        .then(ConvertParametersData)
         .catch((error) => {
-            throw new Error(error);
-        })
-        .then(ConvertParametersData);
+            console.error(error);
+        });
+
+    if (!parameters?.length) return;
 
     let param = {} as {
         maxValue: number;
         maxDecimals: number;
         currency: string;
         paymentMethods: string[];
-        lastModif: Date;
+        lastModified: string;
     };
 
     const param0 = parameters.at(0);
@@ -36,13 +39,17 @@ export async function LoadData() {
     const param1 = parameters.at(1)?.split(',');
     param.paymentMethods = param1?.length && param1.every((item) => !/\d/.test(item)) ? param1 : [];
 
-    param.lastModif = new Date(parameters.at(2) ?? 0);
+    param.lastModified = parameters.at(2) ?? '0';
 
-    const products = await fetch(getData('Produits', 'fetchProducts'))
+    const products = await fetch(
+        isOnline ? './api/spreadsheet?sheetName=Produits&isRaw=true' : './api/json?fileName=products'
+    )
+        .then(ConvertProductsData)
         .catch((error) => {
-            throw new Error(error);
-        })
-        .then(ConvertProductsData);
+            console.error(error);
+        });
+
+    if (!products?.length) return;
 
     let inventory = [] as InventoryItem[];
     products.forEach((item) => {
@@ -76,7 +83,7 @@ async function ConvertParametersData(response: Response) {
     return await response.json().then((data: { values: string[][]; error: { message: string } }) => {
         if (!data) throw new Error('data not fetched');
         if (data.error?.message) throw new Error(data.error.message);
-        if (!data.values || data.values.length === 0) throw new Error('missing data pattern');
+        if (data.values?.length === 0) throw new Error('missing data pattern');
 
         return data.values.map((item) => {
             return item[1];
@@ -88,7 +95,7 @@ async function ConvertProductsData(response: Response) {
     return await response.json().then((data: { values: (string | number)[][]; error: { message: string } }) => {
         if (!data) throw new Error('data not fetched');
         if (data.error && data.error.message) throw new Error(data.error.message);
-        if (!data.values || data.values.length === 0) throw new Error('missing data pattern');
+        if (data.values?.length === 0) throw new Error('missing data pattern');
         const labels = data.values[0];
         return data.values
             .filter((item, i) => i !== 0)
@@ -101,17 +108,4 @@ async function ConvertProductsData(response: Response) {
                 };
             });
     });
-}
-
-function getBaseURL(path?: string | URL) {
-    const base = window ? window.location.protocol + '//' + window.location.host : '';
-    return path ? new URL(path, base) : new URL(base);
-}
-
-function getData(sheetName: string, fallbackAPI: string, isRaw = true) {
-    return GOOGLE_SPREADSHEET_ID && GOOGLE_API_KEY && navigator?.onLine
-        ? `https://sheets.googleapis.com/v4/spreadsheets/${GOOGLE_SPREADSHEET_ID}/values/${sheetName}!A%3AZ?${
-              isRaw ? 'valueRenderOption=UNFORMATTED_VALUE&' : ''
-          }key=${GOOGLE_API_KEY}`
-        : `${getBaseURL()}/api/${fallbackAPI}`;
 }
