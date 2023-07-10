fa17baa8236c0f821af3becd3211465f2fdedf59
Flo <flodef@free.fr>
2023-07-10 03:23:37 +0200

    Add an env file to have environment settings

---
+++ src/app/utils/env.ts
@@ -0,0 +1,5 @@
+export const GOOGLE_SPREADSHEET_ID = process.env.NEXT_PUBLIC_GOOGLE_SPREADSHEET_ID;
+export const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
+export const OTHER_KEYWORD = 'Autres';
+export const CATEGORY_SEPARATOR = '>';
+export const DEFAULT_DATE = [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()].join('-');
