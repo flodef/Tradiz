1a82710d50ae4d824ec271f5104638f64ad019f3
Flo <flodef@free.fr>
2023-07-23 03:32:33 +0200

    Add more constants

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,6 +1,13 @@
-import { PublicKey } from '@solana/web3.js';
+import { PublicKey, clusterApiUrl } from '@solana/web3.js';
 
 export const OTHER_KEYWORD = 'Autres';
 export const CATEGORY_SEPARATOR = '>';
 export const DEFAULT_DATE = [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()].join('-');
 export const MEMO_PROGRAM_ID = new PublicKey('MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr');
+export const IS_DEV = process.env.NEXT_PUBLIC_IS_DEV === 'true';
+export const SPL_TOKEN = new PublicKey(
+    IS_DEV ? 'J99D2TvHcev22FF8rNfdUXQx31qzuoVdXRpRiPzJCH6c' : 'Pnsjp9dbenPeFZWqqPHDygzkCZ4Gr37G8mgdRK2KjQp'
+);
+export const ENDPOINT = IS_DEV
+    ? clusterApiUrl('devnet')
+    : process.env.NEXT_PUBLIC_CLUSTER_ENDPOINT || 'https://solana-mainnet.rpc.extrnode.com';
