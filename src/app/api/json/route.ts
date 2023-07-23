d3334bba831e3aa1c886f8943cdd1aca3c141d91
Flo <flodef@free.fr>
2023-07-23 03:23:02 +0200

    Rework json data mirroring xls changes

--- src/app/api/json/route.ts
+++ src/app/api/json/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import parameters from '../data/parameters.json';
+import paymentMethods from '../data/paymentMethods.json';
 import products from '../data/products.json';
 
 export async function GET(request: Request) {
@@ -8,6 +9,8 @@ export async function GET(request: Request) {
     switch (fileName) {
         case 'parameters':
             return NextResponse.json(parameters);
+        case 'paymentMethods':
+            return NextResponse.json(paymentMethods);
         case 'products':
             return NextResponse.json(products);
         default:
