d1dcbfa16c688702197bf61fcf0580081876673a
Flo <flodef@free.fr>
2023-10-21 01:27:45 +0200

    Add a fullscreen button to set fullscreen when it's not 4/n

--- src/app/components/NumPad.tsx
+++ src/app/components/NumPad.tsx
@@ -295,7 +295,7 @@ export const NumPad: FC = () => {
             !isPopupOpen &&
             (height < window.screen.availHeight || width < window.screen.availWidth)
         ) {
-            openFullscreenPopup('Plein écran', ['Mettre en plein écran'], () => closePopup());
+            openFullscreenPopup('Plein écran', ['Mettre en plein écran'], () => closePopup(requestFullscreen), true);
         }
     }, [openFullscreenPopup, isPopupOpen, height, width, state, closePopup]);
 
