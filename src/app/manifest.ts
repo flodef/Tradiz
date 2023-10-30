c74c3c5cdf537d4d994939e0a57afc9b3c45bf23
Flo <flodef@free.fr>
2023-10-31 00:50:12 +0100

    Improve manifest and use next standard

---
+++ src/app/manifest.ts
@@ -0,0 +1,34 @@
+import { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'FiMs POS - Caisse Enregistreuse',
+        short_name: 'FiMs POS',
+        description: 'Caisse Enregistreuse pour les commerçants permettant de gérer les ventes et les stocks',
+        start_url: '/',
+        scope: '/',
+        lang: 'fr',
+        display: 'standalone',
+        display_override: ['window-controls-overlay'],
+        orientation: 'any',
+        background_color: '#3367D6',
+        theme_color: '#3367D6',
+        icons: [
+            {
+                src: '/favicon.ico',
+                sizes: '128x128',
+                type: 'image/x-icon',
+            },
+            {
+                src: '/images/icons-192.png',
+                type: 'image/png',
+                sizes: '192x192',
+            },
+            {
+                src: '/images/icons-512.png',
+                type: 'image/png',
+                sizes: '512x512',
+            },
+        ],
+    };
+}
