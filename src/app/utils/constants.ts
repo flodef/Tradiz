578a102e817a7f37775d0040e56f0e01d9f8d035
Flojito <flojito@archireport.com>
2025-06-01 21:23:32 +0200

    build: improve IS_DEV const checking

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -22,7 +22,7 @@ export const GET_FORMATTED_DATE = (date = new Date(), precision = 3) =>
 
 // Solana
 export const MEMO_PROGRAM_ID = new PublicKey('MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr');
-export const IS_DEV = process.env.NEXT_PUBLIC_IS_DEV === 'true';
+export const IS_DEV = process.env.NEXT_PUBLIC_IS_DEV?.toLowerCase() === 'true';
 export const SPL_TOKEN = new PublicKey(
     IS_DEV ? 'J99D2TvHcev22FF8rNfdUXQx31qzuoVdXRpRiPzJCH6c' : 'Pnsjp9dbenPeFZWqqPHDygzkCZ4Gr37G8mgdRK2KjQp'
 );
